196b9ec4ab5cb212f348ed8fdbfa207e2b525ac3
LeaSlot <sophiadewaard@gmail.com>
2023-08-29 18:06:30 +0200

    logo agenda gefixt en titel op berichtenpagina

--- agenda.php
+++ agenda.php
@@ -1,18 +1,22 @@
-<?php
-/*
-Template Name: agenda
-*/
-get_header();
-?>
+<head>
+    <?php
+    /*
+    Template Name: agenda
+    */
+    get_header();
+    ?>
+</head>
 
-<body <?php body_class('agenda'); ?>>
+<body>
+
+
 
 <div class ="site-wrapper">
         <div class ="site-content">    
             <div class = "agenda">
                 <?php
-                    if (have_posts()) :
-                        while (have_posts()) : the_post();
+                    if (have_posts()) {
+                        while (have_posts()) { the_post();
                 ?>
                 <article class = "tekst" <?php post_class(); ?>>
                     <h2><?php the_title(); ?></h2>
@@ -21,10 +25,10 @@ get_header();
                     </div>
                 </article>
                 <?php
-                    endwhile;
-                else :
+                }}
+                else {
                     echo '<p>Geen berichten gevonden.</p>';
-                endif;
+                }
                 ?>
             </div>
                         
